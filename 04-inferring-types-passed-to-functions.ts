255ee4989fbfc0187bc2bed1ef63099cada68f35
mowalky <mowalky@outlook.com>
2023-01-13 21:39:42 -0600

    updated

--- 04-inferring-types-passed-to-functions.ts
+++ 04-inferring-types-passed-to-functions.ts
@@ -1,6 +1,15 @@
-const addIdtoObject = <T>(obj: T): T & { id: string } => {
+// You don't have to always pass the types to a generic function
+
+const addIdtoObject = <TObj>(obj: TObj) => {
   return {
     ...obj,
     id: "123",
   };
 };
+
+const result = addIdtoObject({
+  firstName: "Gabe",
+  lastName: "Durand",
+});
+
+console.log(result);
